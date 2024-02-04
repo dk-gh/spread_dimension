fixed propagation of error formula -- pseudo spread dimension returns approximation of standard error for computing confidence intervals

spread_dimension/metric_space.py
@@ -176,7 +176,7 @@ class MetricSpace():
         return np.sum(1/np.sum(E, axis=0))
 
     @staticmethod
-    def _propagation_of_error(mean_X, varX, mean_Y, varY, covXY):
+    def _propagation_of_error(mean_X, varX, mean_Y, varY, covXY, n):
         """Calculates the propagation of error for the pseudo
         spread dimension.
         """
@@ -190,7 +190,9 @@ class MetricSpace():
         D = 2*covXY/(mean_X*mean_Y)
         var = A*(B+C-D)
 
-        return var
+        std_error = np.sqrt(var)/np.sqrt(n)
+
+        return std_error
 
     def _pseudo_spread_part_eval(self, t):
         """Partially evaluates the pseudo spread at scale t,
@@ -253,15 +255,16 @@ class MetricSpace():
 
         G = mean_X/mean_Y
 
-        varG = MetricSpace._propagation_of_error(
+        std_error = MetricSpace._propagation_of_error(
             mean_X,
             varX,
             mean_Y,
             varY,
-            covXY
+            covXY,
+            n
         )
 
-        return G, varG
+        return G, std_error
 
     def spread_dimension(self, t):
         """Returns the spread dimension of distance matrix

tests/tests_metric_space.py
@@ -195,12 +195,23 @@ class Tests(unittest.TestCase):
             2.60514397018874
         )
 
+
+    def test_pseudo_spread_dimension_coincides(self):
+        dist_mat = DM
+        ms = MetricSpace(dist_mat)
+        ms.select_partial_submatrix([0,1,2,3,4,5,6,7,8,9])
+        pseudo_spread_dim = ms.pseudo_spread_dimension(2)[0]
+        pseudo_spread_dim = ms.pseudo_spread_dimension(1)[0]
+        spread_dim = ms.spread_dimension(1)
+
+        self.assertAlmostEqual(spread_dim, pseudo_spread_dim)
+
     def test_pseudo_spread_dimension(self):
         dist_mat = DM
         ms = MetricSpace(dist_mat)
         ms.select_partial_submatrix([0,3,5,8])
-        pseudo_spread_dim = ms.pseudo_spread_dimension(2)
-        pseudo_spread_dim = ms.pseudo_spread_dimension(1)
+        pseudo_spread_dim = ms.pseudo_spread_dimension(2)[0]
+        pseudo_spread_dim = ms.pseudo_spread_dimension(1)[0]
 
         self.assertEqual(
             ms.partial_distance_matrix.shape,
@@ -208,8 +219,8 @@ class Tests(unittest.TestCase):
         )
         self.assertAlmostEqual(
             np.sum(pseudo_spread_dim),
-            0.8386878673024255
-            )
+            0.8220757426879661
+        )
 
     def test_validate_distance_matrix_triang_fail(self):
 